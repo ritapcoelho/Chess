a9e9a073aeea1f9b813b7e87d185321cf0bd4b0b
Miguel Serra <Miguel.Serra@criticaltechworks.com>
2019-12-22 21:29:26 +0000

    Add basic front-end to test add-game/join-game

--- client/chess-app/src/App.js
+++ client/chess-app/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Connection from './Connection';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      namePicked: false
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.setState({namePicked: true})
+  }
+
+  render() {
+    let bod;
+
+    if (!this.state.namePicked) {
+      bod = <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    } else {
+      bod = <Connection name={this.state.value}></Connection>
+    }
+
+  return <div>{bod}</div>
+  }
 }
 
 export default App;
---
+++ client/chess-app/src/Connection.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './App.css';
+
+let URL = 'ws://localhost:8003/chess/'
+
+class Connection extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: []
+    }
+  }
+
+  ws = new WebSocket(URL + this.props.name);
+  
+  componentDidMount() {
+    this.ws.onopen = () => {
+      // on connecting, do nothing but log it to the console
+      console.log('connected')
+    }
+    this.ws.onmessage = evt => {
+      // on receiving a message, add it to the list of messages
+      const message = JSON.parse(evt.data)
+      console.log(message);
+      this.setState({messages: [...this.state.messages, evt.data]})
+    }
+  }
+
+  startGameMessage = messageString => {
+    // on submitting the ChatInput form, send the message, add it to the list and reset the input
+    const message = { messageType: 'ADD_GAME', color: 'WHITE' }
+    this.ws.send(JSON.stringify(message))
+  }
+
+  joinGameMessage = messageString => {
+    // on submitting the ChatInput form, send the message, add it to the list and reset the input
+    const message = { messageType: 'JOIN_GAME' }
+    this.ws.send(JSON.stringify(message))
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.startGameMessage}>New Game</button>
+        <button onClick={this.joinGameMessage}>Join Game</button>
+        {this.state.messages.map(txt => <p>{txt}</p>)}
+      </div>
+    )
+  }
+}
+
+export default Connection;
